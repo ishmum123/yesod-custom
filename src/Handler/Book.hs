310a6cb438bc6fd563266303fbae732e62b51ffc
Ishmum Jawad Khan <ishmum@cokreates.com>
2020-03-25 01:57:20 +0600

    [feature] delete & get-single added

--- src/Handler/Book.hs
+++ src/Handler/Book.hs
@@ -10,6 +10,21 @@ postBookR = do
 
 getBookR :: Handler Value
 getBookR = do
+  books <- runDB $ selectList ([] :: [Filter Book]) []
+  returnJson books
+
+getBookSingleR :: Key Book -> Handler Value
+getBookSingleR bookId = do
+  book <- runDB $ selectFirst [BookId ==. bookId] []
+  returnJson book
+
+putBookSingleR :: Key Book -> Handler Value
+putBookSingleR _ = do
   let x = selectList ([] :: [Filter Book]) []
   books <- runDB x
   returnJson books
+
+deleteBookSingleR :: Key Book -> Handler Value
+deleteBookSingleR bookId = do
+  _ <- runDB $ delete bookId
+  returnJson ""
--- test/Handler/BookSpec.hs
+++ test/Handler/BookSpec.hs
@@ -13,7 +13,9 @@ spec :: Spec
 spec =
   withApp $ do
     creationSpec
-    retrievalSpec
+    bulkRetrievalSpec
+    singleRetrievalSpec
+    singleDeletionSpec
     invalidRequestSpec
 
 creationSpec :: SpecWith (TestApp App)
@@ -33,6 +35,7 @@ creationSpec =
       setRequestBody encoded
       addRequestHeader ("Content-Type", "application/json")
     statusIs 200
+    printBody
     books <- runDB $ selectList [BookName ==. name] []
     Entity _id book <-
       case books of
@@ -40,9 +43,9 @@ creationSpec =
         _     -> error "needed 1 entity"
     assertEq "Should have " book (Book name author isPirated Nothing)
 
-retrievalSpec :: SpecWith (TestApp App)
-retrievalSpec =
-  describe "retrieval request" $
+bulkRetrievalSpec :: SpecWith (TestApp App)
+bulkRetrievalSpec =
+  describe "bulk retrieval request" $
   it "gets all the books" $ do
     let book1 = Book "Introduction..." "Coremen" False Nothing
         book2 = Book "Calculus" "Anton" False Nothing
@@ -50,9 +53,42 @@ retrievalSpec =
     _ <- runDB $ insertEntity book2
     get BookR
     statusIs 200
+    printBody
     (books :: [Book]) <- requireJSONResponse
     assertEq "Should have 2 book" 2 $ P.length books
     assertEq "Should be the same book" book1 $ P.head books
+
+singleRetrievalSpec :: SpecWith (TestApp App)
+singleRetrievalSpec =
+  describe "single retrieval request" $
+  it "gets book with id" $ do
+    let book1 = Book "Introduction..." "Coremen" False Nothing
+        book2 = Book "Calculus" "Anton" False Nothing
+    _ <- runDB $ insertEntity book1
+    _ <- runDB $ insertEntity book2
+    get $ BookSingleR $ BookKey 2
+    statusIs 200
+    printBody
+    (book :: Book) <- requireJSONResponse
+    assertEq "Should be the same book" book2 book
+
+singleDeletionSpec :: SpecWith (TestApp App)
+singleDeletionSpec =
+  describe "single deletion request" $
+  it "should remove book with id" $ do
+    let book1 = Book "Introduction..." "Coremen" False Nothing
+        book2 = Book "Calculus" "Anton" False Nothing
+        book3 = Book "Pattern...." "Bishop" False Nothing
+    _ <- runDB $ insertEntity book1
+    _ <- runDB $ insertEntity book2
+    _ <- runDB $ insertEntity book3
+    performMethod "DELETE" $ BookSingleR $ BookKey 1
+    statusIs 200
+    printBody
+    books <- runDB $ selectList ([] :: [Filter Book]) []
+    assertEq "Should decrease count" 2 $ P.length books
+
+-- TODO: PUT REQUEST
 
 invalidRequestSpec :: SpecWith (TestApp App)
 invalidRequestSpec =
